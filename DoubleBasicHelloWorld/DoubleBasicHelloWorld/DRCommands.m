6d428586ddba98b2c25495682edf0d06bdf32874
Jim Andersson <11jimandersson11@gmail.com>
2015-11-24 20:57:20 +0100

    Create DRCommands.m

---
+++ DoubleBasicHelloWorld/DoubleBasicHelloWorld/DRCommands.m
@@ -0,0 +1,17 @@
+//
+//  DRCommands.m
+//  DoubleBasicHelloWorld
+//
+//  Created by Jim Andersson on 2015-25-05.
+//  Copyright (c) 2015 Double Robotics, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "DRCommands.h"
+
+@implementation DRCommands
+@synthesize timing;
+@synthesize type;
+@synthesize queueNr;
+
+@end
